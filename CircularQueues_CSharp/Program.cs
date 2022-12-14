e002cef2cc6d7f02d4c3c7cc9e9aa22001157476
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-12-14 17:10:34 +0700

    add public void insert

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -11,6 +11,20 @@ namespace CircularQueues_CSharp
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
         public Queues()
-        { }
+        {
+            /*Initializzing the values of vatiable REAR and FRONT to -1
+             to indicate that the queue is initially empty.*/
+            FRONT = -1;
+            REAR = -1;
+        }
+        public void insert(int element)
+        {
+            /* This statement checks for the overflow conditions. */
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
+        }
     }
 }

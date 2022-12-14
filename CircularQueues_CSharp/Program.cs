48811b2ce7a2cd4378d95870e45caf5982a8c0bd
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-12-14 17:05:35 +0700

    add method public Queues

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -7,5 +7,10 @@ using System.Threading.Tasks;
 namespace CircularQueues_CSharp
 {
     class Queues
-    {}
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        public Queues()
+        { }
+    }
 }

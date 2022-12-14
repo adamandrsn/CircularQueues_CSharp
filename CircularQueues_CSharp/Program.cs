e2c6c6df84bb76d1b5477869ccc742718ba9380a
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-12-14 17:56:53 +0700

    add command

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -34,21 +34,26 @@ namespace CircularQueues_CSharp
             }
             else
             {
+                /* If REAR is at the last position of the array, then the value of REAR is set to 0 that corresponds to the first position of the array. */
                 if (REAR == max - 1)
                     REAR = 0;
                 else
+                    /* If REAR is not the last position, the its value is incremented by one. */
                     REAR = REAR + 1;
             }
+            /* Once the position of REAR is determined,  the element is added at its proper place. */
             queue_array[REAR] = element;
         }
         public void remove()
         {
+            /* Checks whether the queue is empty. */
             if (FRONT == -1)
             {
                 Console.WriteLine("Queue underflow\n");
                 return;
             }
             Console.WriteLine("\nThe element deleted from the queue is: " + queue_array[FRONT] + "\n");
+            /* Checks if the queue has one element. */
             if (FRONT == REAR)
             {
                 FRONT = -1;
@@ -56,9 +61,13 @@ namespace CircularQueues_CSharp
             }
             else
             {
+                /* If the element to be deleted is at the last position
+                 of the array, then the value of FRONT is set to 0 i.e
+                 to the first element of the array */
                 if (FRONT == max - 1)
                     FRONT = 0;
                 else
+                    /* FRONT is incremented by one if it is not the first element of array. */
                     FRONT = FRONT + 1;
             }
         }
@@ -66,11 +75,13 @@ namespace CircularQueues_CSharp
         {
             int FRONT_position = FRONT;
             int REAR_position = REAR;
+            /*checks if the queue is empty. */
             if (FRONT == -1)
             {
                 Console.WriteLine("\nElements in the queue are.............\n");
                 if (FRONT_position <= REAR_position)
                 {
+                    /* traverses the queue till the last element present in an array. */
                     while (FRONT_position <= REAR_position)
                     {
                         Console.Write(queue_array[FRONT_position] + "    ");
@@ -80,12 +91,15 @@ namespace CircularQueues_CSharp
                 }
                 else
                 {
+                    /* traverses the queue till the last position of the array. */
                     while (FRONT_position <= max - 1)
                     {
                         Console.Write(queue_array[FRONT_position] + "    ");
                         FRONT_position++;
                     }
+                    /* set the FRONT position to the first element of array. */
                     FRONT_position = 0;
+                    /* traverses the array till the last element present in the queue. */
                     while (FRONT_position <= REAR_position)
                     {
                         Console.Write(queue_array[FRONT_position] + "     ");
